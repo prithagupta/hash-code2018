52ca069d5fb846cc5088aaf15dad186c09cf48d2
Pritha Gupta <prithagupta.nsit@gmail.com>
2018-03-01 19:41:49 +0100

    added sorting

--- dataset_parser.py
+++ dataset_parser.py
@@ -4,11 +4,12 @@ import os
 class DataserParser(metaclass=ABCMeta):
 
     def __init__(self):
-        path = os.path.join(os.getcwd(),"data","a_example.in")#
+        path = os.path.join(os.getcwd(),"data","b_should_be_easy.in")#
         df = pd.read_csv(path, header=None, sep=" ")
         matrix = df.as_matrix()
         self.rows, self.cols, self.n_vehicles, self.rides, self.bonus, self.steps = matrix[0]
         self.ride_information = matrix[1:,:]
+        self.ride_information = self.ride_information[self.ride_information[:, 4].argsort()]
 if __name__ == '__main__':
     dr = DataserParser()
     print(dr.rows)
